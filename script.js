658fa6e076e240509eb529f81b91fd6edd8f97e6
Can-Tkcu <can.tkcu@hotmail.com>
2022-09-07 12:12:23 +0200

    adjusted responsive design

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let fields = [];
+let lines = [0,1,2,3,4,5,6,7];
 let winner;
 let gameOver = false;
 let currentShape = "cross";
+let horizontal = "scaleX(0.0)";
+let vertical = "rotate(90deg) scaleX(0.0)";
+let diagonal = "rotate(45deg) scaleX(0.0)";
+let diagonalInv = "rotate(135deg) scaleX(0.0)";
+
 let AUDIO_CROSS = new Audio("audio/cross.mp3");
 AUDIO_CROSS.volume = 0.17;
 let AUDIO_CIRCLE = new Audio("audio/circle.mp3");
@@ -116,36 +122,57 @@ function restartGame() {
   document.getElementById("circle-" + i).classList.add("d-none");
   document.getElementById("cross-" + i).classList.add("d-none");
   }
-  document.getElementById("game-is-over").classList.add("d-none");
-  document.getElementById('restart').classList.add('d-none');
-  currentShape = "cross";
-  winner = undefined;
-  gameOver = false;
-  playerTwoIsActive();
-  resetAllLines();
-  fields = [];
+  hideEndScreen();
+  resetVariables();
+}
+
+function Lines(i, horizontal, vertical, diagonal, diagonalInv) {
+  if (i < 3) {
+    document.getElementById("line-" + i).style.transform = `${horizontal}`;
+  }
+  if (i > 2 && i < 6) {
+    document.getElementById("line-" + i).style.transform = `${vertical}`;  
+  } 
+  if(i == 6) {
+    document.getElementById("line-" + i).style.transform = `${diagonalInv}`;  
+  }
+  if ( i == 7) {
+     document.getElementById("line-" + i).style.transform = `${diagonal}`;  
+  }
 }
 
 function printAllLines() {
-  document.getElementById("line-0").style.transform = "scaleX(1.0)";
-  document.getElementById("line-1").style.transform = "scaleX(1.0)";
-  document.getElementById("line-2").style.transform = "scaleX(1.0)";
-  document.getElementById("line-5").style.transform = "rotate(90deg) scaleX(1.0)";
-  document.getElementById("line-3").style.transform = "rotate(90deg) scaleX(1.0)";
-  document.getElementById("line-4").style.transform = "rotate(90deg) scaleX(1.0)";
-  document.getElementById("line-7").style.transform = "rotate(45deg) scaleX(1.0)";
-  document.getElementById("line-6").style.transform = "rotate(135deg) scaleX(1.0)";
+  for (let i = 0; i < lines.length; i++) {
+    horizontal = "scaleX(1.0)";
+    vertical = "rotate(90deg) scaleX(1.0)";
+    diagonal = "rotate(45deg) scaleX(1.0)";
+    diagonalInv = "rotate(135deg) scaleX(1.0)";
+    Lines(i, horizontal, vertical, diagonal, diagonalInv);
+  }
 }
 
 function resetAllLines(){
-  document.getElementById("line-0").style.transform = "scaleX(0.0)";
-  document.getElementById("line-1").style.transform = "scaleX(0.0)";
-  document.getElementById("line-2").style.transform = "scaleX(0.0)";
-  document.getElementById("line-5").style.transform = "rotate(90deg) scaleX(0.0)";
-  document.getElementById("line-3").style.transform = "rotate(90deg) scaleX(0.0)";
-  document.getElementById("line-4").style.transform = "rotate(90deg) scaleX(0.0)";
-  document.getElementById("line-7").style.transform = "rotate(45deg) scaleX(0.0)";
-  document.getElementById("line-6").style.transform = "rotate(135deg) scaleX(0.0)";
+  for (let i = 0; i < lines.length; i++) {
+    Lines(i, horizontal, vertical, diagonal, diagonalInv);
+  }
+}
+
+function resetVariables() {
+  horizontal = "scaleX(0.0)";
+  vertical = "rotate(90deg) scaleX(0.0)";
+  diagonal = "rotate(45deg) scaleX(0.0)";
+  diagonalInv = "rotate(135deg) scaleX(0.0)";
+  playerTwoIsActive();
+  resetAllLines();
+  winner = undefined;
+  gameOver = false;
+  currentShape = "cross";
+  fields = [];
+}
+
+function hideEndScreen() {
+  document.getElementById("game-is-over").classList.add("d-none");
+  document.getElementById('restart').classList.add('d-none');
 }
 
 function playerOneIsActive() {
@@ -157,5 +184,3 @@ function playerTwoIsActive() {
   document.getElementById("player-1").classList.remove("player-inactive");
   document.getElementById("player-2").classList.add("player-inactive");
 }
-
-
